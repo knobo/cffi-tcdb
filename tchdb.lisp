9a187ad019c32dff4539c70b48c891e2313f3eab
Knut Olav Bøhmer <bohmer@gmail.com>
2010-01-20 13:24:18 +0100

    UTF-8 works. I don't know what I did. Have to check the diff

--- tchdb.lisp
+++ tchdb.lisp
@@ -22,20 +22,22 @@
      (store object store))))
 
 (defun str-to-object (vector)
-  (with-input-from-sequence (s vector)
-    (restore s)))
+  (let ((vect (make-array (length vector) :fill-pointer t :adjustable t :initial-contents vector)))
+    (with-input-from-sequence (s vect)
+      (restore s))))
 
 (defvar *knobo* nil)
 
-(defun mk-vec (vec)
-  (cffi:foreign-alloc :char
-		      :count (length vec)
-		      :initial-contents (string-to-octets vec))) ;;
+(defun string-to-vector (string)
+  (let ((vec (string-to-octets string )))
+    (cffi:foreign-alloc :unsigned-char
+			:count (length vec)
+			:initial-contents vec))) ;;
 
 (defun tcput (db key-arg vector-arg)
-  (let* ((key (mk-vec key-arg))
+  (let* ((key (string-to-vector key-arg))
 	 (key-len (length key-arg))
-	 (val (mk-vec vector-arg))
+	 (val (string-to-vector vector-arg))
 	 (val-len (length vector-arg))
 	 (done (cffi:pointer-address (cl-tc.tc-sys::tchdbput db key key-len val val-len))))
     (case done
@@ -43,7 +45,7 @@
       (0 nil))))
 
 (defun tcget (db key)
-  (let* ((dbkey (mk-vec key))
+  (let* ((dbkey (string-to-vector key))
 	 (dbklen (length key))
 	 (len (cffi:foreign-alloc :int))
 	 (db-result (cl-tc.tc-sys::tchdbget db dbkey dbklen len))
@@ -51,7 +53,7 @@
 	 (result-val (make-array result-len)))
 
     (loop for i below result-len
-       do (setf (aref result-val i) (cffi:mem-aref db-result :char i)))
+       do (setf (aref result-val i) (cffi:mem-aref db-result :unsigned-char i)))
     result-val))
 
 (defun tchdb-store (db key  object) 
@@ -62,23 +64,19 @@
 
 (defclass test-class ()
   ((foo :accessor foo-of :initarg :foo :initform "testing foo")
-   (bar :accessor bar-of :initarg :bar :initform "testing bar")))
+   (bar :accessor bar-of :initarg :bar :initform "testing barå")))
 
 (with-tchdb (db "/tmp/tch")
   (tcput db "test" "1234123412341234")
   (tcget db "test"))
 
 (with-tchdb (db-test "/home/knobo/temp/tchdb")
-  (tc-put db-test "foo" "bar")
+  (tc-put db-test "foo" "baræøå")
   (tc-get db-test "foo"))
 
 (with-tchdb (db-test "/home/knobo/temp/tchdb")
-   (let ((object (make-instance 'test-class)))
-     (tchdb-store db-test "foo" (list "abcdefghijklmnopqrstuvwxyz" :knobo object :test object)) ;; æøå
-     (tchdb-restore db-test "foo")))
+  (let ((object (make-instance 'test-class)))
+    (tchdb-store db-test "åfoo" "æøåobject") ;; æøå
+    (tchdb-restore db-test "åfoo")))
 
 
-(with-tchdb (db-test "/home/knobo/temp/tchdb")
-  (tchdb-store db-test "foo" "xsssabcd")
-  (format t "---~s---~%" (ignore-errors (tchdb-restore db-test "foo")))
-  (tchdb-restore db-test "foo"))
