3512132032418db0049d53bda69a0edeb09dc357
Knut Olav Bøhmer <bohmer@gmail.com>
2010-02-19 14:54:34 +0100

    Removed the tests for the old api

--- tests/test.lisp
+++ tests/test.lisp
@@ -1,37 +1,25 @@
-(defpackage #:tchdb-tests
-  (:use #:cl #:tchdb #:fiveam)
-  (:export :test-tchdb))
+(defpackage #:tcdb-tests
+  (:use #:cl #:tcdb #:fiveam)
+  (:export :test-tcdb))
 
-(in-package #:tchdb-tests)
-
-(defclass empty-test-object ()
-  ())
-
-(defparameter *empty-test-object* (make-instance 'empty-test-object))
-
-(defun store-restore-object (object)
-  (with-tchdb (db "/tmp/test-db")
-    (tchdb-store db "test" object)
-    (tchdb-restore db "test")))
+(in-package #:tcdb-tests)
 
 (def-suite test-db :description "testing the database")
 (in-suite test-db)
 
 (def-fixture dbx/fixture ()
-  (with-tchdb (db "/tmp/test-db")
+  (with-tcdb (db "/tmp/test-db")
     (&body)))
 
-
 (test sore-object 
-  "First test" 
-  (with-fixture dbx/fixture ()
-    (is (eql (type-of (progn  (tchdb-store db "test-object" *empty-test-object*) *empty-test-object*))
-	     (type-of (tchdb-restore db "test-object"))))))
+      "First test" 
+      (with-fixture dbx/fixture ()
+		    ))
 
 (test sore-utf-8-object 
-  "First test"
-  (is (string-equal (store-restore-object "æøå")
-		    "æøå")))
+      "First test"
+      (is (string-equal (store-restore-object "æøå")
+			"æøå")))
 
-(defun test-tchdb ()
+(defun test-tcdb ()
   (explain! (run 'test-db)))
